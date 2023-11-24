6587a3f1c9dae701b0c9de3425146ab9314ee851
KaiokkFernandes <kaiofernandess654@gmail.com>
2023-11-23 23:24:24 -0300

    Implementando classe menu e botao

---
+++ core/src/com/paraproj/game/MenuScreen.java
@@ -0,0 +1,65 @@
+package com.paraproj.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
+import com.paraproj.game.GameConstante;
+import com.paraproj.game.Silveira;
+
+import java.awt.*;
+
+public class MenuScreen implements Screen {
+
+    final Silveira game;
+    private Skin mySkin;
+    private Stage stage;
+
+    public MenuScreen(final Silveira game){
+        this.game = game;
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
