8572d2d5950ecd0ba68294d8ff41794f3d523a3a
AllequiZ <henriqueamc2003@gmail.com>
2023-11-27 20:55:05 -0300

    iniciando o vetor de fotos

---
+++ core/src/com/paraproj/game/LocalFoto.java
@@ -0,0 +1,15 @@
+package com.paraproj.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class LocalFoto {
+    Texture foto;
+    float x;
+    float y;
+
+    public LocalFoto(Texture foto, float x, float y) {
+        this.foto = foto;
+        this.x = x;
+        this.y = y;
+    }
+}
