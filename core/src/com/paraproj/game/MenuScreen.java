c1c7e3373014ea237319393eaee32790daf5c06b
KaiokkFernandes <kaiofernandess654@gmail.com>
2023-11-27 11:09:37 -0300

    Criando meunu e botoes play e settings refatorando rotas

--- core/src/com/paraproj/game/MenuScreen.java
+++ core/src/com/paraproj/game/MenuScreen.java
@@ -12,67 +12,91 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Align;
-import com.paraproj.game.GameConstante;
-import com.paraproj.game.Silveira;
 
-import java.awt.*;
+import javax.print.attribute.TextSyntax;
 
 public class MenuScreen implements Screen {
 
     final Silveira game;
     private Skin mySkin;
     private Stage stage;
+    private Texture TelaFundo;
+    float centerX = Gdx.graphics.getWidth() / 2f;
+    float centerY = Gdx.graphics.getHeight() / 2f;
 
-    public MenuScreen(final Silveira game){
+    public MenuScreen(final Silveira game) {
         this.game = game;
-        mySkin = new Skin(Gdx.files.internal(GameConstante.skin));
+        TelaFundo = new Texture(Gdx.files.internal("FundoMenu.jpeg"));
+        mySkin = new Skin(Gdx.files.internal("skin/glassy-ui.json")); // Caminho do arquivo de skin pode variar
         stage = new Stage(game.screenPort);
-        Label gameTitulo = new Label("GAME MENU", mySkin, "big");
-        gameTitulo.setSize(GameConstante.col_width*2, GameConstante.row_heigth*2);
-        gameTitulo.setPosition(0, GameConstante.centerY);
-        gameTitulo.setAlignment(Align.center);
 
-        stage.addActor(gameTitulo);
+        Label gameTitle = new Label("", mySkin, "big");
+        gameTitle.setSize(200, 100);
+        gameTitle.setPosition(400, 300); // Posicione conforme necessário
+        gameTitle.setAlignment(Align.center);
 
+        Button startBtn = new TextButton("START GAME", mySkin, "small");
+        startBtn.setSize(200, 50);
+        startBtn.setPosition(centerX - startBtn.getWidth()/2, centerY); // Posicione conforme necessário
+
+
+        startBtn.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.gotoGameScreen(); // Chama o método para iniciar o jogo
+                return true;
+            }
+        });
+
+        Button settingsBtn = new TextButton("SETTINGS", mySkin, "small");
+        settingsBtn.setSize(200, 50);
+        settingsBtn.setPosition(centerX - settingsBtn.getWidth()/2, centerY -60); // Posicione conforme necessário
+        settingsBtn.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                // Implemente a lógica para ir para a tela de configurações
+                return true;
+            }
+        });
+
+        stage.addActor(gameTitle);
+        stage.addActor(startBtn);
+        stage.addActor(settingsBtn);
     }
-
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0,1,0,0);
+        Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
+        game.batch.begin();
+        game.batch.draw(TelaFundo, 0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.batch.end();
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
-
     }
 
     @Override
     public void resize(int width, int height) {
-        game.screenPort.update(width,height);
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
-     game.dispose();
+        mySkin.dispose();
+        stage.dispose();
     }
 }
